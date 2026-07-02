7c7fc82e0e989aef128f2d194b1627c298a22071
alex4eto1007 <alex4eto100507@gmail.com>
2026-07-02 07:54:02 +0000

    Fixed db_manager.py - added func import for fee collection

--- database/db_manager.py
+++ database/db_manager.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.pool import QueuePool
 from config.settings import Config
